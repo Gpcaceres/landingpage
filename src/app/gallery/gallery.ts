663df03603e26b2043e126fa2d6a9713575d0d4e
Germán Cáceres <gpcaceres@espe.edu.ec>
2025-10-07 23:23:48 -0500

    upt

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -29,29 +29,19 @@ export class Gallery {
       image: 'https://placehold.co/320x200/60A5FA/ffffff?text=App+Movil',
       alt: 'Vista previa de una app móvil intuitiva'
     },
-    {
-      title: 'Identidad de marca creativa',
-      description: 'Sistema de branding y guía de estilos para startups tecnológicas.',
-      image: 'https://placehold.co/320x200/34D399/ffffff?text=Diseno+Grafico',
-      alt: 'Diseño gráfico enfocado en identidad de marca'
-    },
-    {
-      title: 'Plataforma e-commerce',
-      description: 'Arquitectura completa para comercio electrónico con integración de pagos.',
-      image: 'https://placehold.co/320x200/FCD34D/111827?text=E-commerce',
-      alt: 'Captura de una tienda en línea'
-    },
+ 
     {
       title: 'Dashboard de analíticas',
       description: 'Visualización de métricas clave en tiempo real para equipos de producto.',
       image: 'https://placehold.co/320x200/FB923C/ffffff?text=Analitica',
       alt: 'Panel de analíticas con gráficos y estadísticas'
     },
-    {
-      title: 'Campaña de marketing digital',
-      description: 'Automatización de campañas multicanal con seguimiento de KPIs.',
-      image: 'https://placehold.co/320x200/EF4444/ffffff?text=Marketing',
-      alt: 'Representación de una campaña de marketing digital'
-    }
+   {
+  title: 'Articulo de Redes',
+  description: 'Constructing and Editing Directed Follower Networks from Twitter (X) Reply Threads: An Interactive Visualisation Tool',
+  image: 'https://placehold.co/320x200/EF4444/ffffff?text=Artículo',
+  alt: 'Representación de una campaña de marketing digital'
+  }
+
   ];
 }
